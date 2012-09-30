da7574fb5ba09460958138961447213e9b8294b9
Anthony Apollo Nardi <anthonygt@gmail.com>
2012-09-30 12:39:21 -0400

    improved legibility and the module now differentiates between dom elements and other objects

--- events.js
+++ events.js
@@ -1,88 +1,99 @@
-var events = (function (window, document, undefined) {
+moduleLoader.imports("events", [], function () {
 
-    var returnObject = {},
-        list = [];
+  var returnObject = {},
+  list = [];
 
-    var on = function (name, callback) {
-        
-        if (typeof list[name] === "undefined") {
-        
-            window.addEventListener(name, fire);
-            
-            list.push(name);
-            list[name] = [];
-            list[name].push([this, callback]);
-        
-        } else { list[name].push([this, callback]); }
-
-        return this;
-    };
-
-    var off = function (name, callback, opt) {
-
-        var event = list[name],
-            events = event.length,
-            i = 0;
-
-        if (!events) { return this; }
-
-        if (opt) { window.removeEventListener(name, fire); }
-
-        for (i; i < events; i += 1) {
-            if (event[i][0] === this && event[i][1] === callback) {
-                event.splice(i, 1);
-            }
-        }
-
-        return this;
-
-    };
-
-    var fire = function (e) {
-
-        var event = undefined,
-            data = undefined,
-            events = undefined,
-            current = undefined,
-            i = 0;
-
-        if (typeof e === "string") {
-
-            name = e;
-            data = arguments[1];
-        
-        } else {
-            
-            name = e.type;
-            data = e;
-       
-        }
-
-        event = list[name];
-
-        if (event && event.length) {
-           
-            events = event.length;
-
-            for (i; i < events; i += 1) {
-
-                current = event[i];
-                current[1].apply(current[0], [data]);
-           
-            }
-        }
-
-        return this;
-
-    };
-
-    returnObject = function(name, callback) { return on(name, callback) };
+  var on = function (name, callback) {
+  
+    if (typeof list[name] === "undefined") {
     
-    returnObject.list = list;
-    returnObject.on = on;
-    returnObject.off = off;
-    returnObject.fire = fire;
+      console.log(typeof this)
 
-    return returnObject;
+      if (this instanceof Node) {
 
-}(window, document));
+      this.addEventListener(name, fire);
+
+      } else {
+
+      window.addEventListener(name, fire);
+      
+      }
+      
+      list.push(name);
+      list[name] = [];
+      list[name].push([this, callback]);
+    
+    } else { list[name].push([this, callback]); }
+
+    return this;
+  
+  };
+
+  var off = function (name, callback, opt) {
+
+    var event = list[name],
+      events = event.length,
+      i = 0;
+
+    if (!events) { return this; }
+
+    if (opt) { window.removeEventListener(name, fire); }
+
+    for (i; i < events; i += 1) {
+      if (event[i][0] === this && event[i][1] === callback) {
+      event.splice(i, 1);
+      }
+    }
+
+    return this;
+
+  };
+
+  var fire = function (e) {
+
+    var event = undefined,
+      data = undefined,
+      events = undefined,
+      current = undefined,
+      i = 0;
+
+    if (typeof e === "string") {
+
+      name = e;
+      data = arguments[1];
+    
+    } else {
+      
+      name = e.type;
+      data = e;
+     
+    }
+
+    event = list[name];
+
+    if (event && event.length) {
+       
+      events = event.length;
+
+      for (i; i < events; i += 1) {
+
+      current = event[i];
+      current[1].apply(current[0], [data]);
+       
+      }
+    }
+
+    return this;
+
+  };
+
+  returnObject = function(name, callback) { return on(name, callback) };
+  
+  returnObject.list = list;
+  returnObject.on = on;
+  returnObject.off = off;
+  returnObject.fire = fire;
+
+  return returnObject;
+
+});
